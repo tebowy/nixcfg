1e7abf7cc7391ab0b760be74f8fddb716324a2e3
anon <anon@anon.anon>
2023-03-31 15:36:28 +0200

    xd

--- nixos/albert-configuration.nix
+++ nixos/albert-configuration.nix
@@ -10,8 +10,8 @@
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware):
-       inputs.hardware.nixosModules.common-cpu-amd
-      inputs.hardware.nixosModules.common-ssd
+    #    inputs.hardware.nixosModules.common-cpu-amd
+    #   inputs.hardware.nixosModules.common-ssd
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
