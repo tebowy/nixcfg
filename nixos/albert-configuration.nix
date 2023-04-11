a74bea11e19161c522f505a92abc12c704ef106a
Tebowy Seba <3144357+tebowy@users.noreply.github.com>
2023-04-11 11:06:32 +0200

    fusemnt

--- nixos/albert-configuration.nix
+++ nixos/albert-configuration.nix
@@ -121,6 +121,8 @@
   };
 
   security.sudo.wheelNeedsPassword = false;
+  
+  programs.fuse.userAllowOther = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
